83018a256ccb072f9682b6c147403eb15b33ba7a
Tofade gbenga <drcraig20@gmail.com>
2016-06-30 11:38:27 +0100

    modified grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,7 +113,7 @@ module.exports = function (grunt) {
           '{.tmp,<%= yeoman.client %>}/{app,components}/**/*.js',
           '!{.tmp,<%= yeoman.client %>}{app,components}/**/*.spec.js',
           '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.mock.js',
-          '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,mp4,webp,svg}'
         ],
         options: {
           livereload: true
