fb4c0b215767405a43d518d6580935002fd88baf
Omonayajo Oladapo <o.omonayajo@gmail.com>
2016-07-30 04:37:25 +0100

    Cache candidates list

--- server/api/contestants/contestants.controller.js
+++ server/api/contestants/contestants.controller.js
@@ -2,9 +2,32 @@
 
 var Contestant = require('./contestants.model');
 
+var redis = require('redis'),
+  config = require('../../config/environment'),
+  redisClient = redis.createClient(config.redis.uri);
+
 exports.getContestants = function (req, res) {
-  Contestant.find({}).sort('fullname').exec(function (err,contestants) {
-    return res.json(contestants);
+  const CACHE_KEY = "CONTESTANTS";
+  
+  redisClient.exists(CACHE_KEY, function (err, response) {
+    if (err) { return doDefault(); }
+    else {
+      if (response == 1) {
+        redisClient.get(CACHE_KEY, function (err, contestants) {
+          return res.json(JSON.parse(contestants));
+        });
+      } else {
+        return doDefault();
+      }
+    }
   });
+  
+  function doDefault() {
+    Contestant.find({}).sort('fullname').exec(function (err,contestants) {
+      redisClient.set(CACHE_KEY, JSON.stringify(contestants), function () {
+        return res.json(contestants);
+      });
+    });
+  }
 };
 
